271d8d4d6450ef3bf99da5f0770efc49053af856
mallou <maelle.lenoble@hotmail.fr>
2021-02-17 12:12:44 +0100

    time to eat

--- TP/JSTP4/worker.js
+++ TP/JSTP4/worker.js
@@ -1,9 +1,9 @@
 onmessage = function(e) {
     console.log('Worker: Message received from main script');
-    //const rand_x = parseInt(getRandomInt(600));
-    //const rand_y = parseInt(getRandomInt(600));
+    const rand_x = parseInt(getRandomInt(600));
+    const rand_y = parseInt(getRandomInt(600));
 
-    //const workerResult = 'Result x : ' + rand_x + 'Result y : ' + rand_y;
+    const workerResult = 'Result x : ' + rand_x + 'Result y : ' + rand_y;
     console.log('Worker: Posting message back to main script');
     postMessage(workerResult);
     
