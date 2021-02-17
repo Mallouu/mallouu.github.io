006de39149d8e6d5c1195190c742728da03eaac1
mallou <maelle.lenoble@hotmail.fr>
2021-02-17 12:07:50 +0100

    add worker.js

---
+++ TP/JSTP4/worker.js
@@ -0,0 +1,10 @@
+onmessage = function(e) {
+    console.log('Worker: Message received from main script');
+    //const rand_x = parseInt(getRandomInt(600));
+    //const rand_y = parseInt(getRandomInt(600));
+
+    //const workerResult = 'Result x : ' + rand_x + 'Result y : ' + rand_y;
+    console.log('Worker: Posting message back to main script');
+    postMessage(workerResult);
+    
+}
